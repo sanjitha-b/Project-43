c35c44b7a7e2235e3dd0f099882f491c63e6c4b8
sanjitha-b <71564780+sanjitha-b@users.noreply.github.com>
2021-04-24 13:31:47 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,180)
+    fill("lightblue")
+    text("Hint: Stores all information !! ", 100,200)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 500,70)
+    fill("lightblue")
+    text("Hint: Performs a paticular task !!  ", 500,90)
+
+}
